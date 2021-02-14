e28d0688a6a48f6245ee5380f56103fc6203fbf2
Jim Smits <james.smits@gmail.com>
2021-02-14 15:58:49 -0500

    very much just proof of concept

--- ConsoleExpressions/Program.cs
+++ ConsoleExpressions/Program.cs
@@ -6,23 +6,39 @@ namespace ConsoleExpressions
     {
         static int _width;
         static int _height;
+        static Point _target;
         public struct Point {
             public static int x;
             public static int y;
+            public int X;
+            public int Y;
         }
         static void Main(string[] args)
         {
+            _width = Console.WindowWidth;
+            _height = Console.WindowHeight;
+
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.Clear();
+            SetTarget(new Random().Next(0, _width), new Random().Next(0, _height));
             while (true)
             {
-                int ceilingX = Console.WindowWidth;
-                int ceilingY = Console.WindowHeight;
-                Point.x = new Random().Next(0, ceilingX);
-                Point.y = new Random().Next(0, ceilingY);
 
-                SetCursor();
-                Color();
-                Stroke(1);
-                System.Threading.Thread.Sleep(5);
+                string input = Console.ReadLine();
+                Fire(input);
+                ////Speak(input);
+                //int ceilingX = Console.WindowWidth;
+                //int ceilingY = Console.WindowHeight;
+                //Point.x = new Random().Next(0, ceilingX);
+                //Point.y = new Random().Next(0, ceilingY);
+
+                //SetCursor();
+                //Color();
+                //Stroke(1);
+
+
+                //System.Threading.Thread.Sleep(5);
+
                 //    Color();
                 //    Stroke(1);
 
@@ -52,6 +68,36 @@ namespace ConsoleExpressions
             }
         }
 
+        private static void Fire(string input)
+        {
+            Console.SetCursorPosition(int.Parse(input.Split(" ")[0]), int.Parse(input.Split(" ")[1]));
+            Color(ConsoleColor.Blue);
+            Stroke(1);
+            Home();
+            if (Console.CursorLeft == _target.X && Console.CursorTop == _target.Y)
+            {
+                Console.WriteLine("Hit!!!");
+            }
+            else
+            {
+                Console.WriteLine("Miss!");
+            }
+        }
+
+        private static void SetTarget(int v1, int v2)
+        {
+            _target = new Point { X = v1, Y = v2 };
+            SetCursor(v1, v2);
+            Color(ConsoleColor.Red);
+            Stroke(1);
+            Home();
+        }
+
+        private static void Home()
+        {
+            SetCursor(0,0);
+        }
+
         private static void SetCursor()
         {
             Console.SetCursorPosition(Point.x, Point.y);
@@ -67,13 +113,15 @@ namespace ConsoleExpressions
             Console.SetCursorPosition(x, y);
         }
 
-        static void Color()
+        static void Color(ConsoleColor color)
         {
-            Console.BackgroundColor = (ConsoleColor)new Random().Next(0, 15);
+            Console.BackgroundColor = color;
+            //Console.BackgroundColor = (ConsoleColor)new Random().Next(0, 15);
         }
 
         static void Speak(object utterance)
         {
+            SetCursor(0, 0);
             Console.WriteLine(utterance);
         }
     }
